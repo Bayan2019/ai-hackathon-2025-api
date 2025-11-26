e1316a5ab8bcbbef6a9699c90a6472b5aa41e058
Bayan2019 <saparbayevabt@gmail.com>
2025-11-26 17:24:33 +0500

    controllers -- /v1/clients [get] endpoint

---
+++ controllers/2-clients.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Bayan2019/ai-hackathon-2025-api/views"
+)
+
+// GetClients godoc
+// @Tags Users
+// @Summary      Get User profile
+// @Accept       json
+// @Produce      json
+// @Param Authorization header string true "Bearer AccessToken"
+// @Success      200  {object} views.Client "OK"
+// @Failure   	 401  {object} views.ErrorResponse "No token Middleware"
+// @Failure   	 404  {object} views.ErrorResponse "Not found User Middleware"
+// @Failure   	 500  {object} views.ErrorResponse "Couldn't Get user"
+// @Router       /v1/clients [get]
+// @Security Bearer
+func (uh *UsersHandlers) GetClients(w http.ResponseWriter, r *http.Request, user views.User) {
+	clients, err := uh.DB.GetClients(r.Context())
+	if err != nil {
+		views.RespondWithError(w, http.StatusInternalServerError, "couldn't get clients", err)
+		return
+	}
+	views.RespondWithJSON(w, http.StatusOK, views.DatabaseClients2viewClients(clients))
+}
