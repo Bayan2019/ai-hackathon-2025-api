be2f172ee1d4e7dd8781f5a507aced65387adac5
Bayan2019 <saparbayevabt@gmail.com>
2025-11-26 17:23:52 +0500

    views -- clients

---
+++ views/2-clients.go
@@ -0,0 +1,36 @@
+package views
+
+import "github.com/Bayan2019/ai-hackathon-2025-api/repositories/database"
+
+type Client struct {
+	CstDimID  int64  `json:"cst_dim_id"`
+	Gender    string `json:"gender"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+
+func databaseClient2viewClient(dbC database.Client) Client {
+	return Client{
+		CstDimID:  dbC.CstDimID,
+		Gender:    dbC.Gender,
+		FirstName: dbC.FirstName,
+		LastName:  dbC.LastName,
+	}
+}
+
+func DatabaseClients2viewClients(dbCs []database.Client) []Client {
+	clients := []Client{}
+	for _, c := range dbCs {
+		clients = append(clients, databaseClient2viewClient(c))
+	}
+	return clients
+}
