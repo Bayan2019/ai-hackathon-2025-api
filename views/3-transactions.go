a9fe2ef439be0d90241a3e553b22b27b08d25ee5
Bayan2019 <saparbayevabt@gmail.com>
2025-11-26 19:16:23 +0500

    views -- transactions

--- views/3-transactions.go
+++ views/3-transactions.go
@@ -1,11 +1,40 @@
 package views
 
-import "time"
+import (
+	"github.com/Bayan2019/ai-hackathon-2025-api/repositories/database"
+)
 
 type Transaction struct {
-	FullNameClient string    `json:"full_name_of_sender"`
-	Transdatetime  time.Time `json:"transdatetime"`
-	Amount         int64     `json:"amount"`
-	Destination    string    `json:"destination"`
-	Target         string    `json:"target"`
+	FullNameClient string `json:"full_name_of_sender"`
+	Transdatetime  string `json:"transdatetime"`
+	Amount         int64  `json:"amount"`
+	Destination    string `json:"destination"`
+	Target         int64  `json:"target"`
+}
+
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+//// accommodating functions ////
+
+func databaseGetTransactionsRow2viewTransaction(dbT database.GetTransactionsRow) Transaction {
+	return Transaction{
+		FullNameClient: dbT.FirstName.String + " " + dbT.LastName.String,
+		Transdatetime:  dbT.Transdatetime,
+		Amount:         dbT.Amount,
+		Destination:    dbT.Direction,
+		Target:         dbT.Target,
+	}
+}
+
+func DatabaseGetTransactionsRows2viewTransactions(dbTs []database.GetTransactionsRow) []Transaction {
+	transactions := []Transaction{}
+	for _, t := range dbTs {
+		transactions = append(transactions, databaseGetTransactionsRow2viewTransaction(t))
+	}
+	return transactions
 }
